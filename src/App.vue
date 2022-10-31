5104b51fb2c43d3d05165f8739aeee51448c0b66
Abdool-Zaid <aztoorabally7447@gmail.com>
2022-10-31 09:25:45 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+let i;
+let centerCoords;
+let main = () => {
+  navigator.geolocation.getCurrentPosition((position) => {
+    centerCoords = {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    };
+    if(localStorage.centerCoords){
+      console.log(centerCoords.longitude- JSON.parse(localStorage.centerCoords).longitude,
+      centerCoords.latitude-JSON.parse(localStorage.centerCoords).latitude
+      )
+    }
+    console.log('clicked')
+localStorage.centerCoords?localStorage.centerCoords:
+localStorage.setItem('centerCoords',JSON.stringify(centerCoords) )
+
+  });
+};
+</script>
+
+<template>
+  <button class="btn btn-primary" @click="main()">run</button>
+</template>
+
+<style scoped></style>
